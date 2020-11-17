4d27781c773cefc4e7614a0fd8656d139813810b
IsmailG80 <ismailrose78@gmail.com>
2020-11-17 20:31:30 +0100

    2.gun 2. commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -3,6 +3,7 @@ public class BirinciGun {
 	public static void main (String[]args) {
 		System.out.println("11:57 - Birinci Gun");
 		System.out.println("20:15 - Ikinci Gun");
+		System.out.println("20:26 2. gun");
 		
 
 }
