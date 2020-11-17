49f285d59a33d7f42c38bfe6251aa083f5631f57
IsmailG80 <ismailrose78@gmail.com>
2020-11-17 21:33:58 +0100

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -5,9 +5,9 @@ public class BirinciGun {
 		System.out.println("20:15 - Ikinci Gun");
 		System.out.println("20:26 2. gun");
 		System.out.println("20:35 2. gun, Git hub dan");
-
 		System.out.println("21:04 2. gun, Github.com");
 		System.out.println("21:23 2. gun, github.com");
+		System.out.println("burada conflict beklemiyorum");
 
 		
 
