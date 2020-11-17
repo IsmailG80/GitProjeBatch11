f536266f507d2258526cc9f2a544b1f6ea4ed2e7
IsmailG80 <74257344+IsmailG80@users.noreply.github.com>
2020-11-17 20:36:50 +0100

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -4,6 +4,7 @@ public class BirinciGun {
 		System.out.println("11:57 - Birinci Gun");
 		System.out.println("20:15 - Ikinci Gun");
 		System.out.println("20:26 2. gun");
+		System.out.println("20:35 2. gun, Git hub dan");
 		
 
 }
